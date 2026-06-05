93cd6ef4d429a753a9b2889e82aacea9df387de1
Jolmer2004 <136146542+Jolmer2004@users.noreply.github.com>
2026-06-05 01:51:44 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Yared', 'Londoño', 'yared@yemm.com', 'yared123', 'SCRUM MASTER');
-INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Stefany', 'Izquierdo', 'stefany@yemm.com', 'stefany123', 'DESARROLLADOR');
-INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Marko', 'Arcila', 'marko@yemm.com', 'marko123', 'PRODUCT OWNER');
-INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Esteban', 'Triviño', 'esteban@yemm.com', 'esteban123', 'DESARROLLADOR');
-INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Jolmer', 'Viedma', 'jolmer@yemm.com', 'jolmer123', 'DESARROLLADOR');
+INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Yared', 'Londoño', 'yared@gmail.com', 'yared123', 'SCRUM MASTER');
+INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Stefany', 'Izquierdo', 'stefany@gmail.com', 'stefany123', 'DESARROLLADOR');
+INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Marko', 'Arcila', 'marko@gmail.com', 'marko123', 'PRODUCT OWNER');
+INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Esteban', 'Triviño', 'esteban@gmail.com', 'esteban123', 'DESARROLLADOR');
+INSERT INTO usuarios (nombre, apellido, email, password, rol) VALUES ('Jolmer', 'Viedma', 'jolmer@gmail.com', 'jolmer123', 'DESARROLLADOR');
